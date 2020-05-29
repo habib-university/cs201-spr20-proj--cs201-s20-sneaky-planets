5db589145fd29f3336dee258f79bda3d1be48afa
AmmarKhalid123 <ammarkhalid237@gmail.com>
2020-05-30 00:27:57 +0500

    Initializing Files

---
+++ rangeTree.py
@@ -0,0 +1,34 @@
+#implementation of range tree
+
+class Node:
+    def __init__(data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class OneDimRt:
+    def __init__(orderBy):
+        self.root = None
+        self.order = order
+    
+    def add(node):
+        if self.root == None:
+            self.root = node
+        else:
+            if self.order == "name":
+                temp = self.root
+                
+
+
+
+class RangeTree:
+    def __init__():
+        self.root = None
+        lines = open("data.txt", "r")
+        for line in lines:
+            employeeInfo = line.split()
+            employee = Node(data) #data is a list where 0: name, 1: age, 2: position, 3: salary
+            
+
+    def add(node):
+        #add new employee in range tree
