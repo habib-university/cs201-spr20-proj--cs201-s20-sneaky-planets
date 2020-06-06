0b7344436f4698979df0998520893b514aee4593
AmmarKhalid123 <ammarkhalid237@gmail.com>
2020-06-06 20:41:11 +0500

    2D add implemented

--- rangeTree.py
+++ rangeTree.py
@@ -1,5 +1,47 @@
 #range tree implementation - not completed
 
+def printLeaf(root):
+  if root is None:
+    return
+  if root.left is None and root.right is None:
+    print(root.data)
+    return
+  if root.left is not None:
+    printLeaf(root.left)
+  if root.right is not None:
+    printLeaf(root.right)
+
+def printLevelOrder(root): 
+      
+    # Base case 
+    if root is None: 
+        return
+    # Create an empty queue for level order traversal 
+    q = [] 
+      
+    # Enqueue root and initialize height 
+    q.append(root) 
+          
+    while q: 
+      
+        # nodeCount (queue size) indicates number 
+        # of nodes at current lelvel. 
+        count = len(q) 
+          
+        # Dequeue all nodes of current level and  
+        # Enqueue all nodes of next level  
+        while count > 0: 
+            temp = q.pop(0) 
+            print(temp.data, end = ' ') 
+            if temp.left: 
+                q.append(temp.left) 
+            if temp.right: 
+                q.append(temp.right) 
+  
+            count -= 1
+        print(' ') 
+
+
 class OneDeeNode:
 	def __init__(self, data=None):
 		self.data = data
@@ -15,9 +57,10 @@ class HigherDeeNode:
 		self.other = None
 
 class OneDeeRangeTree:
-	def __init__(self):
+	def __init__(self, data=None):
 		self.root = OneDeeNode()
-
+		if data != None:
+			self.add(data, self.root)
 	def add(self, node, root):
 		if self.root.data == None:
 			self.root.data = node.data
@@ -63,66 +106,48 @@ class OneDeeRangeTree:
 			self.rangeSearch(start, end, results, root.right)
 
 class TwoDeeRangeTree:
-	def __init__(self, data=None):
+	def __init__(self):
 		self.root = HigherDeeNode()
 	
-	def add(self, node, root):
+	def add(self, node, roott):
 		if self.root.data == None:
 			self.root.data = node.data
 			self.root.left = HigherDeeNode(node.data)
-			oneDee = OneDeeRangeTree()
-			oneDee.add(node, oneDee.root)
-			self.root.other = oneDee
+			self.root.left.other = OneDeeRangeTree(node)
+			self.root.other = OneDeeRangeTree(node)
 			return
 		
+		if roott.data == None:
+			roott.data = node.data
+			return
 		
-# lines = open("data.txt", "r")
+		temp = roott
 
-# for line in lines:
-# 	employeeInfo = line.split()
-# 	employee = Node(data) #data is a list where 0: id, 1: age, 2: position, 3: salary, 4: name
+		if node.data[1] <= temp.data[1]:
+			if temp.left is None:
+				temp.left = HigherDeeNode()
+				temp.right = HigherDeeNode(temp.data)
+				temp.data = node.data
+				if temp.other == None:
+					temp.other = OneDeeRangeTree()
+			temp.other.add(node, temp.other.root)
+			self.add(node, temp.left)
+		if node.data[1] > temp.data[1]:
+			if temp.right is None:
+				temp.right = HigherDeeNode()
+				temp.right.other = OneDeeRangeTree(node)
+				temp.right.left = HigherDeeNode(node.data)
+			temp.other.add(node, temp.other.root)
+			self.add(node, temp.right)
+
+lines = open("data.txt", "r")
+
+a = TwoDeeRangeTree()
 
 
+for line in lines:
+	employeeInfo = line.split()
+	node = HigherDeeNode(employeeInfo)
+	a.add(node, a.root)
 
-
-def printLeaf(root):
-  if root is None:
-    return
-  if root.left is None and root.right is None:
-    print(root.data)
-    return
-  if root.left is not None:
-    printLeaf(root.left)
-  if root.right is not None:
-    printLeaf(root.right)
-
-def printLevelOrder(root): 
-      
-    # Base case 
-    if root is None: 
-        return
-    # Create an empty queue for level order traversal 
-    q = [] 
-      
-    # Enqueue root and initialize height 
-    q.append(root) 
-          
-    while q: 
-      
-        # nodeCount (queue size) indicates number 
-        # of nodes at current lelvel. 
-        count = len(q) 
-          
-        # Dequeue all nodes of current level and  
-        # Enqueue all nodes of next level  
-        while count > 0: 
-            temp = q.pop(0) 
-            print(temp.data, end = ' ') 
-            if temp.left: 
-                q.append(temp.left) 
-            if temp.right: 
-                q.append(temp.right) 
-  
-            count -= 1
-        print(' ') 
-
+printLeaf(a.root)
